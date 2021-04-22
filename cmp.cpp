e4c7abc6078c5ec4925ce2e03a63901f23d4abb0
AbdurRahim-dev <raahimm11@gmail.com>
2021-04-22 15:37:34 +0600

    Compare Function of c++

---
+++ cmp.cpp
@@ -0,0 +1,9 @@
+bool cmp(const int &a , const int &b){
+	return a > b ; 
+}
+//Use any other variable in place of (int)
+
+//we can sort this way too in sort func... 
+
+sort(x.begin() , x.end() ,greater<char>()) ; //char or some other variable. 
+ 
