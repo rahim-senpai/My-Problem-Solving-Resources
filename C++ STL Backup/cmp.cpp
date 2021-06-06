3050dea5b590262ee738b2402c38ae0c3d177a22
AbdurRahim-dev <raahimm11@gmail.com>
2021-06-06 20:26:47 +0600

    Update Compare function

--- C++ STL Backup/cmp.cpp
+++ C++ STL Backup/cmp.cpp
@@ -5,5 +5,8 @@ bool cmp(const int &a , const int &b){
 
 //we can sort this way too in sort func... 
 
-sort(x.begin() , x.end() ,greater<char>()) ; //char or some other variable. 
+///sort(x.begin() , x.end() ,greater<char>()) ; //char or some other variable. 
  
+bool cmpp(const int &a , const int &b){
+	return a % 2 < b % 2 ; // Even first , odd last 
+}
